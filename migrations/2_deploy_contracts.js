6e857f987fd4d2ec21bdf70380d82d6ff8709c4c
DavdRoman <d@vidroman.me>
2017-06-20 18:57:45 +0100

    Gardening

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,5 @@
-var ConvertLib = artifacts.require("./ConvertLib.sol");
-var MetaCoin = artifacts.require("./MetaCoin.sol");
+const Token = artifacts.require('./Token.sol')
 
 module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, MetaCoin);
-  deployer.deploy(MetaCoin);
-};
+	deployer.deploy(Token)
+}
