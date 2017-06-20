a23d9b1a2f8105db6d7cfab2e99493134ced8f1f
DavdRoman <d@vidroman.me>
2017-06-20 19:30:46 +0100

    Clean up PayrollInterface

--- contracts/PayrollInterface.sol
+++ contracts/PayrollInterface.sol
@@ -1,27 +1,25 @@
 pragma solidity ^0.4.11;
 
-// For the sake of simplicity lets asume USD is a ERC20 token
-// Also lets asume we can 100% trust the exchange rate oracle
 contract PayrollInterface {
-	/* OWNER ONLY */
+	// Company (owner) functions
 	function addEmployee(address accountAddress, address[] allowedTokens, uint256 initialYearlyUSDSalary);
 	function setEmployeeSalary(uint256 employeeId, uint256 yearlyUSDSalary);
 	function removeEmployee(uint256 employeeId);
 
 	function addFunds() payable;
 	function scapeHatch();
-	// function addTokenFunds()? // Use approveAndCall or ERC223 tokenFallback
+	function addTokenFunds()
 
 	function getEmployeeCount() constant returns (uint256);
-	function getEmployee(uint256 employeeId) constant returns (address employee); // Return all important info too
+	function getEmployee(uint256 employeeId) constant returns (address employee);
 
-	function calculatePayrollBurnrate() constant returns (uint256); // Monthly usd amount spent in salaries
-	function calculatePayrollRunway() constant returns (uint256); // Days until the contract can run out of funds
+	function calculatePayrollBurnrate() constant returns (uint256);
+	function calculatePayrollRunway() constant returns (uint256);
 
-	/* EMPLOYEE ONLY */
-	function determineAllocation(address[] tokens, uint256[] distribution); // only callable once every 6 months
-	function payday(); // only callable once a month
+	// Employee functions
+	function determineAllocation(address[] tokens, uint256[] distribution);
+	function payday();
 
-	/* ORACLE ONLY */
-	function setExchangeRate(address token, uint256 usdExchangeRate); // uses decimals from token
+	// Exchange rate Oracle callbacks
+	function setExchangeRate(address token, uint256 usdExchangeRate);
 }
