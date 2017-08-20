d4ba771d7ad7ba3c03cf771b930656c59f47832f
DavdRoman <d@vidroman.me>
2017-08-20 21:15:58 +0100

    Disable deployment

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,11 +1,17 @@
-const Ownable = artifacts.require('Ownable.sol')
+// const Ownable = artifacts.require('Ownable.sol')
+// const HumanStandardToken = artifacts.require('HumanStandardToken.sol')
+// const USDExchange = artifacts.require('USDExchange.sol')
+// const EmployeesController = artifacts.require('EmployeesController.sol')
 // const Payroll = artifacts.require('Payroll.sol')
-const EmployeesController = artifacts.require('EmployeesController.sol')
 
 module.exports = function(deployer) {
-	deployer.deploy(Ownable)
+	// deployer.deploy(Ownable)
 	// deployer.link(Ownable, Payroll)
-	deployer.link(Ownable, EmployeesController)
+	// deployer.link(Ownable, EmployeesController)
+	// deployer.link(IHumanStandardToken, USDExchange)
 	// deployer.deploy(Payroll)
-	deployer.deploy(EmployeesController)
+	// deployer.deploy(EmployeesController)
+	// deployer.deploy(StandardToken);
+	// deployer.deploy(HumanStandardToken);
+	// deployer.deploy(USDExchange);
 }
