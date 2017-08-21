3601561a24fa9adff306318a2d6c097be5ecfeda
DavdRoman <d@vidroman.me>
2017-08-21 04:17:15 +0100

    Add base Payroll

--- contracts/Payroll/IPayroll.sol
+++ contracts/Payroll/IPayroll.sol
@@ -1,10 +1,15 @@
 pragma solidity ^0.4.11;
 
 contract IPayroll {
-	function daysUntilNextPayday() constant returns(int);
-	function payday();
+	// company
+	function calculatePayrollBurnrate() constant returns (uint);
+	function calculatePayrollRunway() constant returns (uint);
 
-	function tokensForAllocation() constant returns (address[]);
+	// employee
+	function availableTokensForAllocation() constant returns (address[]);
 	function daysUntilNextAllocation() constant returns(int);
 	function determineAllocation(address[] tokens, uint[] distribution);
+
+	function daysUntilNextPayday() constant returns(int);
+	function payday();
 }
--- contracts/Payroll/Payroll.sol
+++ contracts/Payroll/Payroll.sol
@@ -1,19 +1,32 @@
 pragma solidity ^0.4.11;
 
-import '../Zeppelin/Ownable.sol';
 import './IPayroll.sol';
+import '../Zeppelin/Ownable.sol';
+import '../Employees/EmployeesController.sol';
 
-contract Payroll is Ownable, IPayroll {
+contract Payroll is IPayroll, Ownable {
+	EmployeesController employees;
 
-	function daysUntilNextPayday() constant returns(int) {
+	function Payroll(address initialEmployeesController) {
+		setEmployeesController(initialEmployeesController);
+	}
+
+	function setEmployeesController(address newEmployeesController) onlyOwner {
+		require(newEmployeesController != 0x0);
+		employees = EmployeesController(newEmployeesController);
+	}
+
+	// Monthly usd amount spent in salaries
+	function calculatePayrollBurnrate() constant onlyOwner returns (uint) {
 
 	}
 
-	function payday() {
+	// Days until the contract can run out of funds
+	function calculatePayrollRunway() constant onlyOwner returns (uint) {
 
 	}
 
-	function tokensForAllocation() constant returns (address[]) {
+	function availableTokensForAllocation() constant returns (address[]) {
 
 	}
 
@@ -28,13 +41,14 @@ contract Payroll is Ownable, IPayroll {
 	/// max sum of 10000 (100.00%).
 	/// i.e. [5000, 3050, 1950] (50%, 30.50%, 19.50%)
 	function determineAllocation(address[] tokens, uint[] distributions) {
-		/*require(employees[msg.sender].length != 0);
-		require(tokenAddress.length > 0);
-		require(tokenAddress.length == distribution.length);
+		
+	}
 
-		for (var i = 0; i < tokenAddress.length; i++) {
-			address tokenAddress = tokenAddresses[i];
-			uint distribution = distribution[i];
-		}*/
+	function daysUntilNextPayday() constant returns(int) {
+
+	}
+
+	function payday() {
+
 	}
 }
