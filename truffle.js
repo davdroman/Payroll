0a821471068f3bc3866460344f83b86072728589
DavdRoman <d@vidroman.me>
2017-09-25 04:12:07 +0100

    Add Ropsten testnet

--- truffle.js
+++ truffle.js
@@ -3,16 +3,21 @@ module.exports = {
 		live: {
 			network_id: 1 // Ethereum public network
 		},
-		development: {
+		ropsten: {
 			host: 'localhost',
 			port: 8545,
+			network_id: 3
+	    },
+		development: {
+			host: 'localhost',
+			port: 8546,
 			network_id: '*'
 		},
 		testing: {
 			host: 'localhost',
-			port: 8555,
+			port: 8547,
 			network_id: '*',
 			gasPrice: 0x01
 		}
 	}
-};
+}
