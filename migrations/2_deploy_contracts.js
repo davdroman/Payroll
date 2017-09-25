2fe39a6ef503880d4442b351ead9ef38fc5fb626
DavdRoman <d@vidroman.me>
2017-09-25 06:24:43 +0100

    Gardening

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,8 +4,8 @@ module.exports = (deployer, network) => {
 	const USDExchange = artifacts.require('USDExchange.sol')
 	const Payroll = artifacts.require('Payroll.sol')
 
-	const allocationFrequency = network == 'live' ? 0 : 60; // default for mainnet, 60 seconds for testnets
-	const paydayFrequency = network == 'live' ? 0 : 60; // default for mainnet, 60 seconds for testnets
+	const allocationFrequency = network == 'live' ? 0 : 60 // default for mainnet, 60 seconds for testnets
+	const paydayFrequency = network == 'live' ? 0 : 60 // default for mainnet, 60 seconds for testnets
 
 	deployer.deploy(USDExchange, web3.eth.coinbase).then(() => {
 		return deployer.deploy(Payroll, USDExchange.address, allocationFrequency, paydayFrequency)
