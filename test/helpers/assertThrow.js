6c7f23ad08ad8771de999d10c244a926c8800e2b
DavdRoman <d@vidroman.me>
2017-10-13 14:47:10 +0100

    Enhance `assertThrow` artifact

--- test/helpers/assertThrow.js
+++ test/helpers/assertThrow.js
@@ -1,4 +1,13 @@
-module.exports = function(error) {
+const assertion = error => {
 	const isError = error.message.search('invalid opcode') > -1 || error.message.search('invalid JUMP') > -1
 	assert.isTrue(isError, 'Error expected');
+}
+
+module.exports = async promise => {
+	try {
+		await promise
+	} catch(error) {
+		return assertion(error)
+	}
+	throw new Error('Promise did not throw as expected')
 }
